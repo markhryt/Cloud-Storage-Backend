c5d141480efd493ff91a36458e22ae6913e35e9d
markhryt <hrytchakmark@gmail.com>
2025-06-10 16:54:41 -0400

    Initial Project Setup

---
+++ cloud-storage-backend/src/main/java/com/mh/cloud_storage_backend/CloudStorageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mh.cloud_storage_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStorageBackendApplication.class, args);
+	}
+
+}
---
+++ cloud-storage-backend/src/test/java/com/mh/cloud_storage_backend/CloudStorageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.cloud_storage_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
