f46a090c3769847deab81d02845c900f81257ea4
markhryt <hrytchakmark@gmail.com>
2025-08-02 17:46:54 -0400

    JWT logic changed

--- cloud-storage-backend/src/main/java/com/mh/cloud_storage_backend/model/util/JWTUtil.java
+++ cloud-storage-backend/src/main/java/com/mh/cloud_storage_backend/model/util/JWTUtil.java
@@ -1,17 +1,16 @@
 package com.mh.cloud_storage_backend.model.util;
+
 import com.mh.cloud_storage_backend.model.entities.Users;
 import com.mh.cloud_storage_backend.model.repository.UsersRepo;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -26,40 +25,35 @@ public class JWTUtil {
     @Value("${my.secret.key}")
     private String secretKey;
     private final long expirationTime = 1000 * 60 * 60; // 1 hour in milliseconds
-    private String secret = secretKey;
 
-    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    private final UsersRepo userRepository;
 
     @Autowired
-    private final UsersRepo userRepository;
     public JWTUtil(UsersRepo userRepository) {
         this.userRepository = userRepository;
     }
 
-
     private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(secret.getBytes());
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
     public String generateToken(String username) {
-        String token = Jwts.builder()
+        return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(signatureAlgorithm, getSigningKey())
+                .signWith(getSigningKey())
                 .compact();
-        return token;
     }
 
     public String extractUsername(String token) {
-        Claims body = Jwts.parserBuilder().
-                setSigningKey(getSigningKey())
+        Claims body = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
         return body.getSubject();
     }
-
 
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
@@ -74,10 +68,7 @@ public class JWTUtil {
         try {
             String username = extractUsername(token);
             Optional<Users> user = userRepository.findByEmail(username);
-            if (user.isEmpty()) {
-                return false;
-            }
-            return !isTokenExpired(token);
+            return user.isPresent() && !isTokenExpired(token);
         } catch (Exception e) {
             return false;
         }
@@ -101,6 +92,7 @@ public class JWTUtil {
         }
         return null;
     }
+
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
@@ -108,5 +100,4 @@ public class JWTUtil {
                 .parseClaimsJws(token)
                 .getBody();
     }
-
 }
